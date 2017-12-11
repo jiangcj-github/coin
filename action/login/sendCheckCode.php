d9bba3b18434b4e0b5c94219d2d8ab3391469dfa
lindakai2016 <398017990@qq.com>
2017-12-11 09:48:34 +0800

    ddd

--- action/login/sendCheckCode.php
+++ action/login/sendCheckCode.php
@@ -42,13 +42,22 @@ $code="";
 for ($i=0;$i<6;$i++ ){
     $code.=mt_rand(0,9);
 }
-
-$subject="邮箱验证";
-$message= "<p>本次登录的验证码是:&nbsp;".$code."</p><p>为了确保您的帐号安全，该验证码仅1小时内使用有效，请勿直接回复该邮件。</p>";
-if(!mail($addr,$subject,$message)){
-    die_json(["发送失败"]);
-}
-
+//发送邮件
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "smtp.sina.com";
+$mail->Port = 25;
+$mail->CharSet = "UTF-8";
+$mail->FromName = "淘币客";
+$mail->Username = "taobike_site@sina.com";
+$mail->Password = "20090928";
+$mail->From = "lindakai@taobik.com";
+$mail->isHTML(true);
+$mail->addAddress($addr);
+$mail->Subject = "邮箱验证";
+$mail->Body = "<p>本次登录的验证码是:&nbsp;".$code."</p><p>为了确保您的帐号安全，该验证码仅1小时内使用有效，请勿直接回复该邮件。</p>";
+$status = $mail->send();
 //記錄操作時間
 $stmt=$conn->prepare("insert into checkCode_strict(email,sendCode) values(?,?) ON DUPLICATE KEY update sendCode=?");
 $stri_time=(new DateTime())->format("Y-m-d H:i:s");
